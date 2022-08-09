5c638b69e36a5821cb9383e9e0ba726492dee03e
Marco A. F <marcofurt@gmail.com>
2022-08-08 22:50:55 -0300

    instagram Scrape

--- BussinessLogicalLayer/WebScraperBLL.cs
+++ BussinessLogicalLayer/WebScraperBLL.cs
@@ -8,15 +8,9 @@ namespace BussinessLogicalLayer
 {
     public class WebScraperBLL
     {
-
-        public static List<WebScraperDAL.Datum> Tradingview()
-        {
-            return WebScraperDAL.GetStocks();
-        }
-
         public static List<string> Scrape()
         {
-            return WebScraperDAL.GetStockSymbols();
+            return WebScraperDAL.ScrapeInstagramSelenium("");
         }
     }
 }
--- Teste/Program.cs
+++ Teste/Program.cs
@@ -3,21 +3,10 @@ using BussinessLogicalLayer;
 
 
 Console.WriteLine("Hello, World!");
-/*
-List<string> symbols = WebScraperBLL.Scrape();
 
-for (int i = 0; i < 656; i++)
+foreach (var src in WebScraperBLL.Scrape())
 {
-    System.Console.WriteLine(symbols[i]);
-
-}
-*/
-
-foreach (var item in WebScraperBLL.Tradingview())
-{
-    foreach (var stock in item.D)
-    {
-        System.Console.WriteLine(stock.Symbol + stock.Preco.ToString() + stock.Currency);
-    }
+    System.Console.WriteLine("SOURCE: " + src);
 }
 
+Thread.Sleep(99999999);
