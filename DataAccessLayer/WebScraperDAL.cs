6edb8a230e9a9a622461839dcc3a876f44d3eebe
Marco A. F <marcofurt@gmail.com>
2022-08-19 12:47:42 -0300

    scroll page

--- DataAccessLayer/WebScraperDAL.cs
+++ DataAccessLayer/WebScraperDAL.cs
@@ -1,7 +1,10 @@
+using System.Collections.ObjectModel;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using Shared;
+
 
 
 namespace DataAccessLayer
@@ -43,7 +46,6 @@ namespace DataAccessLayer
             FirefoxOptions options = new();
 
             //makes the browser invisible
-
             if (headless)
             {
                 options.AddArgument("--headless");
@@ -55,51 +57,47 @@ namespace DataAccessLayer
             driver.Navigate().GoToUrl("https://www.instagram.com/");
             Thread.Sleep(4000);
 
-
-            //WebDriverWait wait = new WebDriverWait(driverFox, TimeSpan.FromSeconds(10));
-            //IWebElement firstResult = wait.Until(e => e.FindElement(By.XPath("//a/h3")));
-
-
             //waits and targets the username and password inputs
             WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
-            IWebElement username = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input")));
-            IWebElement password = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input")));
+            IWebElement username = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input")));
+            IWebElement password = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input")));
 
 
             //writes the account's username and password and clicks to login
             username.SendKeys(DotEnv.DEFAULT_USERNAME);
             password.SendKeys(DotEnv.DEFAULT_PASSWORD);
 
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button"))).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button"))).Click();
 
             //handle pop ups
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/section/main/div/div/div/div/button"))).Click();
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]"))).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/section/main/div/div/div/div/button"))).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]"))).Click();
 
             //searches the user profile
-            IWebElement search = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]/input")));
+            IWebElement search = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]/input")));
             search.SendKeys(profile);
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div"))).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div"))).Click();
 
             //scrolls down to scrape more images
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/main/div/div[3]/article/div[1]/div/div[1]/div[1]/a/div/div[2]")));
-            driver.ExecuteScript("window.scrollTo(0, 4000);");
-
-            //NAO CONTINUA SCROLANDO SE A PAGINA N CARREGOU, ARRUMA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-            //targets all images on the page
-            /*var imgs = driver.FindElements(By.TagName("img"));
+            //maybe the index could be a parameter, so the user could define how much they want to scroll
             List<string> sources = new();
 
-            foreach (var img in imgs)
+            for (int i = 1; i < 16; i = i + 4)
             {
-                sources.Add(img.GetAttribute("src").ToString());
+                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/main/div/div[3]/article/div[1]/div/div[" + i + "]")));
+                driver.ExecuteScript("window.scrollTo(0, 4000);");
+
+                //targets all images on the page
+                ReadOnlyCollection<IWebElement> imgs = driver.FindElements(By.TagName("img"));
+
+                foreach (IWebElement img in imgs)
+                {
+                    //TA DANDO EXCEPTION AQUI!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    sources.Add(img.GetAttribute("src").ToString());
+                }
             }
+
             return sources;
-            */
-            return new List<string>();
         }
 
     }
